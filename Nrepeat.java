d5c592a2690b593f419fdc1fe84bdb78ff174bcd
rajeshkumar010 <41105194+rajeshkumar010@users.noreply.github.com>
2018-07-21 12:35:26 +0530

    Create Nrepeat.java

---
+++ Nrepeat.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Nrepeat {
+public static void main(String[] args) {
+	int a;
+	Scanner rk=new Scanner(System.in);
+	System.out.println("Enter the Number: ");
+	a=rk.nextInt();
+	for(int i=0;i<a;i++){
+		System.out.println("Hello");
+	}
+               }
+
+}
