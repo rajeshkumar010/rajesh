9a7e799d6ad0e4e9d63374d4716f2e44249dc1b5
rajeshkumar010 <41105194+rajeshkumar010@users.noreply.github.com>
2018-07-23 17:49:14 +0530

    Create Nrange.java

---
+++ Nrange.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Nrange {
+	public static void main(String args[]) {
+		int a;
+		System.out.println("Enter the Number:");
+		Scanner rk=new Scanner(System.in);
+		a=rk.nextInt();
+		int v[]=new int[10];
+		if(a<10){
+			System.out.println(a);
+			
+		}
+		else{
+			System.out.println("0");
+		}
+		
+	}
+
+}
