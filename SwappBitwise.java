957939b6d064b06f8bef01b56dd4af32b3b08a7a
rajeshkumar010 <41105194+rajeshkumar010@users.noreply.github.com>
2018-07-23 14:25:37 +0530

    Create SwappBitwise.java

---
+++ SwappBitwise.java
@@ -0,0 +1,20 @@
+package rajesh;
+import java.util.Scanner;
+public class SwappBitwise {
+   public static void main(String args[])
+	    {
+	        int m, n;
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter the first number:");
+	        m = s.nextInt();
+	        System.out.print("Enter the second number:");
+	        n = s.nextInt();
+	        m = m ^ n;
+	        n = m ^ n;
+	        m = m ^ n;
+	        System.out.println("After Swapping");
+	        System.out.println("First number:"+m);
+	        System.out.println("Second number:"+n);
+	    }
+	}
+
